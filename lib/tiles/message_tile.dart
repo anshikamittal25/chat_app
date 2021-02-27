4a9d646f5463c42efb76e4236306ae94efe50c33
Anshika Mittal <anshika_m@cs.iitr.ac.in>
2021-02-27 19:31:14 +0530

    modified message tile

--- lib/tiles/message_tile.dart
+++ lib/tiles/message_tile.dart
@@ -14,28 +14,35 @@ class MessageTile extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.all(6.0),
-      child: Container(
-        alignment: byMe ? Alignment.centerRight : Alignment.centerLeft,
-        decoration: BoxDecoration(
-            borderRadius: byMe
-                ? BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                  )
-                : BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
-            color: byMe ? Colors.blue[800] : Colors.pink),
-        padding: EdgeInsets.all(10),
-        child: Text(
-          map['message'],
-          overflow: TextOverflow.visible,
-          textAlign: TextAlign.start,
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ),
+      child: Row(
+        children: [
+          Spacer(),
+          Flexible(
+            child: Container(
+              alignment: byMe ? Alignment.centerRight : Alignment.centerLeft,
+              decoration: BoxDecoration(
+                  borderRadius: byMe
+                      ? BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        )
+                      : BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                  color: byMe ? Colors.blue[800] : Colors.pink),
+              padding: EdgeInsets.all(10),
+              child: Text(
+                map['message'],
+                //overflow: TextOverflow.visible,
+                textAlign: TextAlign.start,
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
